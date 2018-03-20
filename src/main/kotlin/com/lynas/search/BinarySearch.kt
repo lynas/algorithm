a36494a7fd07e8190ba016ce00b4fa0b7fcc2180
LynAs <szlynas@gmail.com>
2018-03-20 21:36:32 +0600

    init

---
+++ src/main/kotlin/com/lynas/search/BinarySearch.kt
@@ -0,0 +1,25 @@
+package com.lynas.search
+
+// complexity O(log n)
+fun binarySearch(array: Array<Int>, arraySize: Int, searchElement: Int): Int {
+    var startPosition = 0
+    var endPosition = arraySize - 1
+
+    while (startPosition <= endPosition) {
+        val mid = (startPosition + endPosition) / 2
+        if (array[mid] == searchElement) {
+            return mid
+        }else if (searchElement < array[mid]) {
+            endPosition = mid - 1
+        } else {
+            startPosition = mid + 1
+        }
+    }
+    return -1
+}
+
+fun main(args: Array<String>) {
+    val array = arrayOf(2,6,13,21,36,47,63,81,97)
+//    val array = arrayOf<Int>()
+    println(binarySearch(array = array,arraySize = array.size,searchElement = 133))
+}
